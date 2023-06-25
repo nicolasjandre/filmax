00579b566039b1bfea0d69376014200dc511bb6c
MICHEL COUTO DO AMPARO <mcouto77@yahoo.com.br>
2023-06-25 10:06:47 -0300

    validacoes inputs

--- src/screens/Cadastrar/styles.ts
+++ src/screens/Cadastrar/styles.ts
@@ -1,22 +1,61 @@
-import { StyleSheet, Platform } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#15141F",
-    padding: 16,
-    paddingTop: 30,
-    paddingBottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    marginTop: Platform.OS === "android" ? "13%" : "20%",
+    marginBottom: Platform.OS === "android" ? "13%" : "15%",
+  },
 
+  input: {
+    width: "90%",
+    height: 42,
+    backgroundColor: "#211F30",
+    marginBottom: 20,
+    padding: 8,
+    borderRadius: 20,
+    borderWidth: 1,
   },
-  text: {
-    color: "#ffffff",
+  forgotContainer: {
+    width: "90%",
+    alignItems: "flex-end",
   },
-    filMAX:{
-      marginTop: Platform.OS === 'android' ? '13%' : '20%',
-      marginButtom: Platform.OS === 'android' ? '13%' : '15%'
-    }
-  
+  forgotText: {
+    color: "#FFFFFF",
+  },
+  loginButton: {
+    marginTop: "5%",
+    backgroundColor: "#FFF",
+    width: "90%",
+    height: 42,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  loginText: {
+    color: "#000000",
+    fontSize: 17,
+  },
+  signUpContainer: {
+    flexDirection: "row",
+    marginTop: "10%",
+  },
+  signUpText: {
+    color: "#C4C4C4",
+    paddingRight: 5,
+  },
+  signUpButtom: {
+    color: "#f00707",
+    fontWeight: "bold",
+  },
+  text: {},
+  conta: {
+    color: "#FFFFFF",
+    fontSize: 20,
+  },
 });
--- src/screens/Entrar/index.tsx
+++ src/screens/Entrar/index.tsx
@@ -1,23 +1,91 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { styles } from "./styles";
 import { NavigationProp } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { StackParamList } from "../../routes";
+import { styles } from "./styles";
 
-interface EntrarProps {
-  navigation: NavigationProp<StackParamList, "Entrar">;
+interface CadastrarProps {
+  navigation: NavigationProp<StackParamList, "Cadastrar">;
 }
 
-export const Entrar = ({ navigation }: EntrarProps) => {
+export const Cadastrar = ({ navigation }: CadastrarProps) => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const validarEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validarSenha = (senha: string) => {
+    const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return senhaRegex.test(senha);
+  };
+
+  const handleCadastrar = () => {
+    const emailValido = validarEmail(email);
+    const senhaValida = validarSenha(senha);
+
+    if (emailValido && senhaValida) {
+      const dadosCadastro = {
+        email: email,
+        senha: senha,
+      };
+
+      // Chamar uma API para realizar o cadastro
+      fetch("https://exemplo.com/api/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dadosCadastro),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Processar a resposta da API
+          console.log("Cadastro realizado com sucesso!");
+          console.log(data);
+        })
+        .catch((error) => {
+          // Lidar com erros durante o cadastro
+          console.error("Erro ao realizar o cadastro:", error);
+        });
+    } else {
+      console.log("Email ou senha inválidos. Por favor, verifique os campos.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Entrar Screen</Text>
-      <Text onPress={() => navigation.navigate("Cadastrar")} style={styles.text}>
-        Voltar para screen Cadastrar
+      <Text onPress={() => navigation.navigate("Entrar")} style={styles.text}>
+        Ir para a tela Entrar
       </Text>
       <Text onPress={() => navigation.navigate("BottomTab", { screen: "Home" })} style={styles.text}>
-        Ir para screen Home
+        Ir para a tela Home
       </Text>
+
+      <StatusBar backgroundColor="#0f0f0f" translucent={false} />
+      <Image source={require("../../assets/images/logo/FilMAX.png")} />
+
+      <View style={styles.conta}>
+        <Text style={styles.conta}>Entrar </Text>
+      </View>
+
+      <TextInput placeholder="Email" style={styles.input} onChangeText={(text) => setEmail(text)} />
+      <TextInput placeholder="Senha" style={styles.input} onChangeText={(text) => setSenha(text)} secureTextEntry />
+
+      <TouchableOpacity style={styles.loginButton} onPress={handleCadastrar}>
+        <Text style={styles.loginText}>Cadastrar</Text>
+      </TouchableOpacity>
+
+      <View style={styles.signUpContainer}>
+        <Text style={styles.signUpText}>Não Possui Conta?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Entrar")}>
+          <Text style={styles.signUpButtom}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
