0f5fff763b6da04dda501a927daf788a495f5ee3
Pedro Viégas <pedrorenatoav1905@gmail.com>
2023-06-27 14:58:47 -0300

    :lipstick: Melhorando Styles da pagina de datalhes e incluindo request pelo axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,89 @@
 import axios, { AxiosResponse } from "axios";
 
+interface MovieData {
+  id: string;
+  title: string;
+  originalTitle: string;
+  fullTitle: string;
+  type: string;
+  year: string;
+  image: string;
+  releaseDate: string;
+  runtimeMins: string;
+  runtimeStr: string;
+  plot: string;
+  plotLocal: string;
+  plotLocalIsRtl: boolean;
+  awards: string;
+  directors: string;
+  directorList: {
+    id: string;
+    name: string;
+  }[];
+  writers: string;
+  writerList: {
+    id: string;
+    name: string;
+  }[];
+  stars: string;
+  starList: {
+    id: string;
+    name: string;
+  }[];
+  actorList: {
+    id: string;
+    image: string;
+    name: string;
+    asCharacter: string;
+  }[];
+  fullCast: any;
+  genres: string;
+  genreList: {
+    key: string;
+    value: string;
+  }[];
+  companies: string;
+  companyList: {
+    id: string;
+    name: string;
+  }[];
+  countries: string;
+  countryList: {
+    key: string;
+    value: string;
+  }[];
+  languages: string;
+  languageList: {
+    key: string;
+    value: string;
+  }[];
+  contentRating: string;
+  imDbRating: string;
+  imDbRatingVotes: string;
+  metacriticRating: string;
+  ratings: any;
+  wikipedia: any;
+  posters: any;
+  images: any;
+  trailer: any;
+  boxOffice: {
+    budget: string;
+    openingWeekendUSA: string;
+    grossUSA: string;
+    cumulativeWorldwideGross: string;
+  };
+  tagline: any;
+  keywords: string;
+  keywordList: string[];
+  similars: {
+    id: string;
+    title: string;
+    image: string;
+    imDbRating: string;
+  }[];
+  errorMessage: string | null;
+}
+
 export type Movie = {
   id: string;
   rank: string;
@@ -16,7 +100,7 @@ interface PopularMovies {
   items: Movie[];
 }
 
-const API_KEY = "k_mfd6clit";
+const API_KEY = "k_h89gtckp";
 
 const api = axios.create({
   baseURL: "https://imdb-api.com/",
@@ -24,4 +108,10 @@ const api = axios.create({
 
 export function getTop250Movies(): Promise<AxiosResponse<PopularMovies, any>> {
   return api.get(`/API/MostPopularMovies/${API_KEY}`);
+}
+
+export function getMovieById(
+  id: string
+): Promise<AxiosResponse<MovieData, any>> {
+  return api.get(`API/Title/${API_KEY}/${id}`);
 }
